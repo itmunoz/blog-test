e5ec9310a812b424c21994a5dc35c9818415adfb
Ignacio Munoz <itmunoz1@uc.cl>
2023-04-18 16:35:44 -0400

    Revoke key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fh=m50ex7!(&nxc%+xdn@0i+j%95t$e50hgg!jj(1fbh$api7='
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
